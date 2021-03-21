e2e020a9c16a6ba1980d780f0e433aaaa9d45aa1
Vostok17 <Gagarin.Artem.2008@gmail.com>
2021-03-22 00:20:10 +0300

    Add push_back()

--- Laba_3/LinkedList.h
+++ Laba_3/LinkedList.h
@@ -2,3 +2,56 @@
 
 #include <iostream>
 #include <string>
+
+template<typename T>
+class List
+{
+public:
+	List();
+
+	void push_back(T data);
+private:
+	template<typename T>
+	class Node
+	{
+	public:
+		Node* pNext;
+		T data;
+		Node(T data = T(), Node* pNext = NULL)
+		{
+			this->data = data;
+			this->pNext = pNext;
+		}
+	};
+
+	int Size;
+	Node<T>* head;
+};
+
+template<typename T>
+List<T>::List()
+{
+	Size = 0;
+	head = NULL;
+}
+
+template<typename T>
+inline void List<T>::push_back(T data)
+{
+	if (head == NULL)
+	{
+		head = new Node<T>(data);
+	}
+	else
+	{
+		Node<T>* current = this->head;
+
+		while (current->pNext != NULL)
+		{
+			current = current->pNext;
+		}
+		current->pNext = new Node<T>(data);
+	}
+
+	Size++;
+}
